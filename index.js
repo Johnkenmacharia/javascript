8a3e540cbbc463c270a337367cad506149f8da1f
Johnkenmacharia <johnkenmacharia@gmail.com>
2023-03-10 10:57:09 +0300

    added changes

---
+++ index.js
@@ -0,0 +1,46 @@
+// create variables
+let name="javascript"
+console.log(name)
+
+let first_name="my"
+let second_name="name"
+let full_name=first_name + second_name
+console.log(full_name)
+
+let num=22
+if (num<10){
+    //code if condition is true
+  console.log("number is less than 10")
+}else{
+    //code if conditon is false
+    console.log("number is greater than 10")
+}
+    
+if else if
+     if (conditon 1){
+        //code if true
+    }else if (conditon 2){
+        //code if false
+    }else if (conditon 3){
+        //code if true
+    }else{
+        //code if condition are false
+    }
+    num_1=15
+    if (num_1>10){
+        console.log("greater than 20")
+    }else if(num_1==10){
+        console.log("less than 10")
+    }
+
+let country ="chad"
+let age = 18
+
+if(country == "togo"){
+    //code if true
+    if(age >=18){
+        console.log("accepted")
+    }else{
+        console.log("not accepted")
+    }
+}
